c45b1920d50a295b33d1d0039d0827c268c4c378
Lestlin Robins <lestlinr@gmail.com>
2025-01-20 15:32:54 +0530

    Added settings page 2

--- src/components/SettingsPage.jsx
+++ src/components/SettingsPage.jsx
@@ -4,7 +4,37 @@ import SignOut from "./SignOut";
 import { supabase } from "../supabase";
 
 function SettingsPage() {
-  return <div>Settings</div>;
+  const [currentUserData, setCurrentUserData] = useState({});
+  const { email, photoURL } = auth.currentUser;
+
+  async function getUsers() {
+    const { data: userData, error } = await supabase
+      .from("users")
+      .select()
+      .eq("emailID", email)
+      .single();
+    if (error) {
+    } else {
+      setCurrentUserData(userData);
+    }
+  }
+  useEffect(() => {
+    async function fetchData() {
+      await getUsers();
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <img src={photoURL} alt="Profile" />
+      <p>{currentUserData.name}</p>
+      <p>{currentUserData.emailID}</p>
+      <p>{currentUserData.hostelName}</p>
+      <p>{currentUserData.roomNo}</p>
+      <SignOut />
+    </div>
+  );
 }
 
 export default SettingsPage;
