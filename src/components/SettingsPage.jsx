69cbf41a80a015e869f7d0ad5f3a4f3dca173828
Lestlin Robins <lestlinr@gmail.com>
2025-06-15 12:27:23 +0530

    refined animations

--- src/components/SettingsPage.jsx
+++ src/components/SettingsPage.jsx
@@ -24,6 +24,7 @@ function SettingsPage({ currentUserData }) {
     }
   };
   const handleTap = () => {
+    navigator.vibrate(50);
     tapCount.current += 1;
 
     // Reset tap count after 1.5s of inactivity
@@ -341,7 +342,16 @@ function SettingsPage({ currentUserData }) {
             }}
           >
             With{" "}
-            <svg
+            <motion.svg
+              whileTap={{
+                scale: 0.75,
+                transition: {
+                  duration: 0.1,
+                  type: "spring",
+                  stiffness: 500,
+                  damping: 20,
+                },
+              }}
               width="40"
               height="40"
               viewBox="0 0 24 24"
@@ -350,7 +360,7 @@ function SettingsPage({ currentUserData }) {
               onClick={handleTap}
             >
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-            </svg>
+            </motion.svg>
           </motion.p>
           <motion.p
             viewport={{ once: true }}
